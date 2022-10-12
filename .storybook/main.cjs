bbbf8857e9166b44372c53df6a8e9795ce682f58
atilio ferreira <atilio.silvaferreira@gmail.com>
2022-10-12 20:26:22 -0300

    fix storybook build show components

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -4,6 +4,7 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
   ],
   framework: "@storybook/react",
   core: {
@@ -11,5 +12,12 @@ module.exports = {
   },
   features: {
     storyStoreV7: true,
+  },
+  viteFinal: (config, { configType }) => {
+    if (configType === "PRODUCTION") {
+      config.base = "/ignite-lab-desing-system";
+    }
+
+    return config;
   },
 };
